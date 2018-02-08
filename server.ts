e409bb295f68e21ad7124518328eddd102c8acee
vic <viktor@screencloud.io>
2018-02-08 15:18:06 +0700

    update

--- server.ts
+++ server.ts
@@ -1,18 +1,26 @@
-const next = require('next')
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
+import * as express from 'express'
+import * as next from 'next'
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-// With express
-const express = require('express')
 app.prepare()
-  .then(() => {
-    const server = express()
-    server.use(handler)
+    .then(() => {
+        const server = express()
+        // CUSTOM ROUTES GO HERE
 
-    const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000
-    server.listen(port, (err: Error) => {
-      if (err) throw err
-      console.log(`> Ready on port ${port}...`)
-    })
-  })
+        // server.get('/blog/:slug', (req: express.Request, res: express.Response) => {
+        //     const mergedQuery = (<any>Object).assign({}, req.query, req.params)
+        //     return app.render(req, res, '/blog', mergedQuery)
+        // });
+        // THIS IS THE DEFAULT ROUTE, DON'T EDIT THIS
+        server.get('*', (req: express.Request, res: express.Response) => {
+            return handle(req, res);
+        });
+        const port = process.env.PORT || 3000;
+
+        server.listen(port, (err: Error) => {
+            if (err) throw err;
+            console.log(`> Ready on port ${port}...`)
+        });
+    });
